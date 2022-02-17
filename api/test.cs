10444a312e86b640d604645ab5b657cdf9913308
Jimmy Segers <segerji@cronos.be>
2022-02-17 19:07:52 +0100

    test

--- api/test.cs
+++ api/test.cs
@@ -34,15 +34,15 @@ namespace JimmyTools.Function
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name ??= data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            var responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new JsonResult(responseMessage);
         }
     }
 }
